d4ac9872a4f4fea722a5bf78661a34764e13acca
Matthew Hasan <matthewhasan@matthews-air.lan>
2024-10-27 18:45:07 -0500

    add endpoints to get from

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ import express from "express";
 import axios from "axios";
 
 const BASE_URL = "https://pokeapi.co/api/v2/";
-const app = express();
 
-app.use(express.static("public"));  
+let id;
+
+const ENDPOINT_1 = `pokemon/${id}`;
+const ENDPOINT_2 = `/pokemon-species/${id}`;
+
+const app = express();
+const port = 3000;
+
+
+
+app.use(express.static("public"));
+
+
+
+app.listen(port, (req, res) => {
+    console.log(`Listening on Port ${port}`);
+});
