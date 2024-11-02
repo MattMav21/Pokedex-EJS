50182b7e3b015a42e019ee31dbb1b1041f591d8d
Matthew Hasan <matthewhasan@matthews-air.lan>
2024-11-01 21:27:46 -0500

    retrieve data needed from pokeapi

--- index.js
+++ index.js
@@ -26,11 +26,49 @@ const pokeData = {
 /// ROUTES
 
 app.get("/", async (req, res) => {
+    
+    try {
+        let id = 1;
+        console.log("HITS");
+        let data1 = await axios.get(BASE_URL + `pokemon/${id}`);
+        let data2 = await axios.get(BASE_URL + `/pokemon-species/${id}`);        
+        
+        data1 = data1.data;
+        data2 = data2.data;
+
+        console.log("DATA Converted!")
+        
+        console.log("DATA 1", data1);
+        console.log("DATA 1: ID", data1.id);
+        console.log("DATA 1: Name", data1.name);
+        console.log("DATA 1: Height", data1.height);
+        console.log("DATA 1: Weight", data1.weight);
+        console.log("DATA 1", data1.sprites.front_default);
+
+
+        console.log("DATA 2: Species", data2.genera[7].genus);
+        console.log("DATA 2: Entry", data2.flavor_text_entries[0].flavor_text);
+
+    } catch (error) {
+        console.error("error");
+        res.render("index.ejs");
+    }
+
+
     res.render("index.ejs");
 });
 
 app.get(`/${id}`, async (req, res) => {
+    // console.log("HITS");
+    // let id = id;
+
+    // let data1 = await axios.get(BASE_URL + ENDPOINT_1);
+    // let data2 = await axios.get(BASE_URL + ENDPOINT_2);
+
+    // console.log("DATA 1", data1);
+    // console.log("DATA 2", data2);
     
+    res.render("index.ejs");
 })
 
 ///
